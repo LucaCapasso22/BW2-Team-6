606fa9f67538794c6a101e8e31fb060b7f213284
Vale-n-tina <valentia.grg@gmail.com>
2024-11-19 14:04:00 +0100

    .

--- album.js
+++ album.js
@@ -1,12 +1,12 @@
 const recuperoDati =function(){
-    fetch(" https://striveschool-api.herokuapp.com/api/deezer/album/75621062")
+    fetch(" https://striveschool-api.herokuapp.com/api/deezer/album/6378419")
     .then((response)=>{
         if(response.ok){
             return response.json()
         }else{throw new Error ("errore nella risppsta")}
     })
-    .then((musics)=>{
-        console.log(musics)
+    .then((album)=>{
+        console.log("questo è l'album",album)
     })
     .catch((error)=>{
         console.log("Orrore",error)
@@ -16,4 +16,27 @@ const recuperoDati =function(){
 recuperoDati()
 
 const addressBarContent=new URLSearchParams(window.location.search)
-addressBarContent.get("albumId")
+addressBarContent.get("albumId");
+
+fetch( "https://striveschool-api.herokuapp.com/api/deezer/album/6378419")
+.then((response)=>{
+    if(response.ok){
+        return response.json()
+    }else{ throw new Error("errore nella risposta")}
+})
+.then((album)=>{
+album.tracks.data.forEach((song)=>{
+    console.log(song)
+    const row=document.getElementById("rowParent")
+   row.innerHTML=`<div class="row">
+            
+            <div class="col col-6">${song.title}<br> <span class="small" > ${song.artist.name}</span></div>
+            <div class="col col-3 d-flex justify-content-around align-items-center">694.578</div>
+            <div class="col col-3 d-flex justify-content-around align-items-center">1:28</div>
+          </div>`
+})
+})
+.catch((error)=>{
+    console.log("orrore", error)
+
+})
