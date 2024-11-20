60704f0dd37712e1ba013e38d6f31a1791ee7778
Vale-n-tina <valentia.grg@gmail.com>
2024-11-20 12:11:08 +0100

    .

--- album.js
+++ album.js
@@ -14,11 +14,11 @@ const recuperoDati =function(){
 }
 
 recuperoDati()
-
+const apiUrl="https://striveschool-api.herokuapp.com/api/deezer/album"
 const addressBarContent=new URLSearchParams(window.location.search)
-addressBarContent.get("albumId");
+let albumId=addressBarContent.get("albumId");
 
-fetch( "https://striveschool-api.herokuapp.com/api/deezer/album/6378419")
+fetch(apiUrl+"/"+ albumId)
 .then((response)=>{
     if(response.ok){
         return response.json()
@@ -26,15 +26,16 @@ fetch( "https://striveschool-api.herokuapp.com/api/deezer/album/6378419")
 })
 .then((album)=>{
     const row=document.getElementById("rowParent")
+    console.log("questo è album", album)
 album.tracks.data.forEach((song)=>{
-    console.log(song)
+    console.log("questa è la song",song)
     const songRow = document.createElement("div");
       songRow.classList.add("row")
    songRow.innerHTML=`
             
-            <div class="col col-6">${song.title}<br> <span class="small" > ${song.artist.name}</span></div>
-            <div class="col col-3 d-flex justify-content-around align-items-center">694.578</div>
-            <div class="col col-3 d-flex justify-content-around align-items-center">1:28</div>
+            <div class="col col-6">${song.title}<br> <a href="#" class="small" > ${song.artist.name}</a></div>
+            <div class="col col-3 d-flex justify-content-around align-items-center">${song.rank}</div>
+            <div class="col col-3 d-flex justify-content-around align-items-center">${formatDuration(song.duration)}</div>
           `
           row.appendChild(songRow)
 })
@@ -43,3 +44,10 @@ album.tracks.data.forEach((song)=>{
     console.log("orrore", error)
 
 })
+
+//FUNZIONE PER TRASFORMARE SECONDI IN MINUTI 
+const formatDuration=function(durationInSeconds) {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = durationInSeconds % 60;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const prova =function(){
+   fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
+   .then((response)=>{
+       if(response.ok){
+           return response.json()
+       }else{throw new Error ("errore nella risposta")}
+   })
+   .then((album)=>{
+       console.log("prova",album)
+       
+       
+   })
+   .catch((error)=>{
+       console.log("Orrore",error)
+   })
+}
+
+prova()
+
+
+//PRIMO ALBUM
 const recuperoDati =function(){
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/1121182")
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/59854962")
     .then((response)=>{
         if(response.ok){
             return response.json()
@@ -11,8 +32,8 @@ const recuperoDati =function(){
         const col=document.createElement("div")
         col.classList.add("col")
         col.innerHTML=`
-                      <div
-                        class="rounded d-flex align-items-center h-70 bg-testo"
+                      <a id="album1" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
                       >
                         <img
                           src="${album.cover}"
@@ -22,8 +43,8 @@ const recuperoDati =function(){
                         <div>
                           <p class="text-light fw-semibold mb-0">${album.title}</p>
                         </div>
-                      </div>
-                    `
+                      </a>`
+                    
                     rowParent.appendChild(col)
     })
     .catch((error)=>{
@@ -32,3 +53,186 @@ const recuperoDati =function(){
 }
 
 recuperoDati()
+
+//SECONDO ALBUM
+const recuperoDati2 =function(){
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/936850")
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{throw new Error ("errore nella risppsta")}
+    })
+    .then((album)=>{
+        console.log("questo è l'album",album)
+        const rowParent=document.getElementById("rowParent")
+        const col=document.createElement("div")
+        col.classList.add("col")
+        col.innerHTML=`
+                      <a id="album2" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
+                      >
+                        <img
+                          src="${album.cover}"
+                          class="w-25 me-2 rounded-start"
+                          alt=""
+                        />
+                        <div>
+                          <p class="text-light fw-semibold mb-0">${album.title}</p>
+                        </div>
+                      </a>`
+                    
+                    rowParent.appendChild(col)
+    })
+    .catch((error)=>{
+        console.log("Orrore",error)
+    })
+}
+
+recuperoDati2()
+
+//TERZO ALBUM
+const recuperoDati3 =function(){
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/124513442")
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{throw new Error ("errore nella risppsta")}
+    })
+    .then((album)=>{
+        console.log("questo è l'album",album)
+        const rowParent=document.getElementById("rowParent")
+        const col=document.createElement("div")
+        col.classList.add("col")
+        col.innerHTML=`
+                      <a id="album3" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
+                      >
+                        <img
+                          src="${album.cover}"
+                          class="w-25 me-2 rounded-start"
+                          alt=""
+                        />
+                        <div>
+                          <p class="text-light fw-semibold mb-0">${album.title}</p>
+                        </div>
+                      </a>`
+                    
+                    rowParent.appendChild(col)
+    })
+    .catch((error)=>{
+        console.log("Orrore",error)
+    })
+}
+
+recuperoDati3()
+
+//QUARTO ALBUM
+const recuperoDati4 =function(){
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/5191431")
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{throw new Error ("errore nella risppsta")}
+    })
+    .then((album)=>{
+        console.log("questo è l'album",album)
+        const rowParent=document.getElementById("rowParent")
+        const col=document.createElement("div")
+        col.classList.add("col")
+        col.innerHTML=`
+                      <a id="album4" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
+                      >
+                        <img
+                          src="${album.cover}"
+                          class="w-25 me-2 rounded-start"
+                          alt=""
+                        />
+                        <div>
+                          <p class="text-light fw-semibold mb-0">${album.title}</p>
+                        </div>
+                      </a>`
+                    
+                    rowParent.appendChild(col)
+    })
+    .catch((error)=>{
+        console.log("Orrore",error)
+    })
+}
+
+recuperoDati4()
+
+//QUINTO ALBUM
+const recuperoDati5 =function(){
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/115018")
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{throw new Error ("errore nella risppsta")}
+    })
+    .then((album)=>{
+        console.log("questo è l'album",album)
+        const rowParent=document.getElementById("rowParent")
+        const col=document.createElement("div")
+        col.classList.add("col")
+        col.innerHTML=`
+                      <a id="album5" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
+                      >
+                        <img
+                          src="${album.cover}"
+                          class="w-25 me-2 rounded-start"
+                          alt=""
+                        />
+                        <div>
+                          <p class="text-light fw-semibold mb-0">${album.title}</p>
+                        </div>
+                      </a>`
+                    
+                    rowParent.appendChild(col)
+    })
+    .catch((error)=>{
+        console.log("Orrore",error)
+    })
+}
+
+recuperoDati5()
+
+//SESTO ALBUM
+const recuperoDati6 =function(){
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/629766")
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{throw new Error ("errore nella risppsta")}
+    })
+    .then((album)=>{
+        console.log("questo è l'album",album)
+        const rowParent=document.getElementById("rowParent")
+        const col=document.createElement("div")
+        col.classList.add("col")
+        col.innerHTML=`
+                      <a id="album6" href="./album.html?albumId=${album.id}"
+                        class="rounded d-flex align-items-center h-70 bg-testo text-decoration-none"
+                      >
+                        <img
+                          src="${album.cover}"
+                          class="w-25 me-2 rounded-start"
+                          alt=""
+                        />
+                        <div>
+                          <p class="text-light fw-semibold mb-0">${album.title}</p>
+                        </div>
+                      </a>`
+                    
+                    rowParent.appendChild(col)
+    })
+    .catch((error)=>{
+        console.log("Orrore",error)
+    })
+}
+
+recuperoDati6()
+
+
+
