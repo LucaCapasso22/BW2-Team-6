5642b0c92db2662b23ee5794b02d2bedcf17ad83
Vale-n-tina <valentia.grg@gmail.com>
2024-11-22 15:18:06 +0100

    .

--- assets/scripts/likeBrani.js
+++ assets/scripts/likeBrani.js
@@ -1,13 +1,13 @@
 let savedSong = localStorage.getItem("savedSong");
-if (savedSong === null  || JSON.parse(savedSong).length === 0) {
+if (savedSong === null || JSON.parse(savedSong).length === 0) {
   const nessunBrano = document.getElementById("nessunBrano");
   nessunBrano.innerHTML = `<h6 class="text-light"> Nessun brano salvato</h6>`;
 } else {
   savedSong = JSON.parse(savedSong);
 
   savedSong.forEach((song) => {
-    const songRow=document.createElement("div")
-    songRow.classList.add("row")
+    const songRow = document.createElement("div");
+    songRow.classList.add("row");
     songRow.innerHTML = `<div class="col col-9 text-light"><a> <i class="fas fa-heart me-2 ${
       savedSong.some((s) => s.title === song.title) ? "text-success" : ""
     } "
@@ -26,36 +26,44 @@ if (savedSong === null  || JSON.parse(savedSong).length === 0) {
          <hr class="text-light mt-3">
             
             `;
-            nessunBrano.appendChild(songRow)
+    nessunBrano.appendChild(songRow);
   });
 }
 
-const cuore = document.querySelectorAll(
-    ".fa-heart:not(.fa-heart.like-icon)"
-  );
-  cuore.forEach((icon) => {
-   
-    icon.addEventListener("click", () => {
-      const title=icon.getAttribute("title-song-id")
-      
-     const name=icon.getAttribute("name-artist-id")
-      const artistId=icon.getAttribute("artist-id")
-      icon.classList.toggle("text-success");
-      let savedSong=localStorage.getItem("savedSong")
-      if(savedSong===null){
-        savedSong=[]
-      }else{savedSong=JSON.parse(savedSong)}
-      const song={
-        title:title,
-        artist:name,
-        artistId:artistId}
-       
-          if(!savedSong.some((songItem)=>songItem.title===song.title)){
-            savedSong.push(song)
-          }else{savedSong=savedSong.filter((songItem)=>songItem.title !==song.title)}
-          console.log("savedSong",savedSong)
-          localStorage.setItem("savedSong",JSON.stringify(savedSong))
-          location.reload()
-        
-    });
+const cuore = document.querySelectorAll(".fa-heart:not(.fa-heart.like-icon)");
+cuore.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const title = icon.getAttribute("title-song-id");
+
+    const name = icon.getAttribute("name-artist-id");
+    const artistId = icon.getAttribute("artist-id");
+    icon.classList.toggle("text-success");
+    let savedSong = localStorage.getItem("savedSong");
+    if (savedSong === null) {
+      savedSong = [];
+    } else {
+      savedSong = JSON.parse(savedSong);
+    }
+    const song = {
+      title: title,
+      artist: name,
+      artistId: artistId,
+    };
+
+    if (!savedSong.some((songItem) => songItem.title === song.title)) {
+      savedSong.push(song);
+    } else {
+      savedSong = savedSong.filter((songItem) => songItem.title !== song.title);
+    }
+    console.log("savedSong", savedSong);
+    localStorage.setItem("savedSong", JSON.stringify(savedSong));
+    location.reload();
   });
+});
+
+const button = document.getElementById("buttonplay");
+const player = document.getElementById("player");
+button.addEventListener("click", () => {
+  player.classList.remove("d-none")
+  
+});
